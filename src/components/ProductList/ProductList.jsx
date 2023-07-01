70273556c871d8183c39f6a6cda840db7b399ca9
horston <machorston@ya.ru>
2023-07-01 09:01:29 +0300

    first commit

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -30,7 +30,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('http://94.26.237.203:8000', {
+        fetch('http://94.26.237.203:800/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
